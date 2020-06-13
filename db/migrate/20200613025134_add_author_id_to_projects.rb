86f41b175fea3c31898602b6bec0efbc396b7126
A L V A R O <alvarop3000@gmail.com>
2020-06-12 21:53:44 -0500

    update db schema

---
+++ db/migrate/20200613025134_add_author_id_to_projects.rb
@@ -0,0 +1,6 @@
+class AddAuthorIdToProjects < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :projects, :author, index: true
+    add_foreign_key :projects, :users, column: :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_200541) do
+ActiveRecord::Schema.define(version: 2020_06_13_025134) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -19,11 +19,12 @@ ActiveRecord::Schema.define(version: 2020_06_12_200541) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.integer "author_id"
     t.string "name"
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_projects_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_06_12_200541) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "projects", "users", column: "author_id"
 end
