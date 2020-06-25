1869145a2d06dd76d0033f52cd6ce947cff39371
A L V A R O <alvarop3000@gmail.com>
2020-06-25 14:59:50 -0500

    Revert "Revert "add ammount to new project""
    This reverts commit e440cf3abaca5dd67a69a0fcd5a42a64697b8ee1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,22 @@ class User < ApplicationRecord
   has_many :projects, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, through: :projects
 
+  def grouped
+    projects.includes(:groups)
+  end
+
   def all_projects
-    projects.includes(:groups).select {|s|s.groups.any?}
+    grouped.select {|s|s.groups.any?}
+  end
+
+  def all_projects_total
   end
 
   def external_projects
-    projects.includes(:groups).reject {|s|s.groups.any?}
+    grouped.reject {|s|s.groups.any?}
   end
+
+  def external_projects_total
+    external_projects.sum(:amount)
+  end 
 end
