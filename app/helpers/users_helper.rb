08a2420df92505307965b74aa23c095679fa5dd2
A L V A R O <alvarop3000@gmail.com>
2020-06-24 23:04:35 -0500

    write additional styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,23 @@
 module UsersHelper
   def menu_action
     if current_user
-      concat(content_tag(:li) do
+      concat(content_tag(:li, class:"profile-menu") do
         link_to 'Profile', user_path(current_user)
       end)
-      concat(content_tag(:li) do
+      concat(content_tag(:li, class:"profile-menu") do
         link_to 'All Projects', projects_path
       end)
-      concat(content_tag(:li) do
+      concat(content_tag(:li, class:"profile-menu") do
         link_to 'All Groups', groups_path
       end)
-      content_tag(:li) do
+      content_tag(:li, class:"profile-menu") do
         link_to 'Logout', logout_path
       end
     else
-      concat(content_tag(:li) do
+      concat(content_tag(:li, class:"profile-menu") do
         link_to 'Login', login_path
       end)
-      content_tag(:li) do
+      content_tag(:li, class:"profile-menu") do
         link_to 'Sign Up', signup_path
       end
     end
