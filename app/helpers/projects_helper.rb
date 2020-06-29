39169677cf3feab5541f88e0058e28f44d147126
A L V A R O <alvarop3000@gmail.com>
2020-06-28 21:39:28 -0500

    delete unnecesary helper on projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,2 @@
 module ProjectsHelper
-  def has_group_total
-    @current_user.project.has_group.group(group_id: params[:group_id]).sum(:amount)
-  end
-
-  def no_group_total
-    @current_user.project.no_group.sum(:amount)
-  end
 end
